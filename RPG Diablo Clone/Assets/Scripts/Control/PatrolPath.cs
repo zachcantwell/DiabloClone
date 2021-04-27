5e8232632376f928c6bf184006ea1e4151273475
zachcantwell <bobbynfld@gmail.com>
2021-04-27 06:09:57 -0230

    OnDrawGizmos for Patrol Paths

---
+++ RPG Diablo Clone/Assets/Scripts/Control/PatrolPath.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPG.Core
+{
+    public class PatrolPath : MonoBehaviour
+    {
+        [SerializeField]
+        private Transform[] _pathPoints;
+
+        private void Awake() {
+            _pathPoints = transform.GetComponentsInChildren<Transform>();    
+        }
+
+        private void OnDrawGizmosSelected() {
+            
+            _pathPoints = transform.GetComponentsInChildren<Transform>();    
+            Gizmos.color = Color.magenta;
+
+            for(int i = 1; i < _pathPoints.Length - 1; i++)
+            {
+                Gizmos.DrawLine(_pathPoints[i].position, _pathPoints[i+1].position);
+            }
+           
+        }
+    }
+}
+
