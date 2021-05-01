f288a5284689371b98b19bb2d90f183cfd897d84
zachcantwell <bobbynfld@gmail.com>
2021-05-01 08:38:44 -0230

    Working on Portals

---
+++ RPG Diablo Clone/Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement
+{
+    public class Portal : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                print("Portal hit");
+                SceneManager.LoadScene(0);
+            }
+        }
+    }
+}
