5849101dc6ef15a4d4c30f5b2b79302cf6cbe5a1
zachcantwell <bobbynfld@gmail.com>
2021-05-01 06:37:27 -0230

    Cinemachine getting improved
    Improved intro cinematic

---
+++ RPG Diablo Clone/Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+namespace RPG.Cinematics
+{
+    public class CinematicTrigger : MonoBehaviour
+    {
+        void OnTriggerEnter(Collider other)
+        {
+            if(other.gameObject.tag == "Player")
+            {
+                GetComponent<PlayableDirector>().Play();
+                GetComponent<BoxCollider>().enabled = false;
+            }
+        }
+    }
+}
