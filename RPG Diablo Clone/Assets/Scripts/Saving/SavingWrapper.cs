f16c7acdad8c58ae6a173a915dc806f3c0a7378b
zachcantwell <bobbynfld@gmail.com>
2021-05-04 03:13:06 -0230

    Imported save / load asset package
    Created a save wrapper for gameobjects

---
+++ RPG Diablo Clone/Assets/Scripts/Saving/SavingWrapper.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using RPG.Saving;
+
+namespace RPG.SaveManagement
+{
+    public class SavingWrapper : MonoBehaviour
+    {
+        const string _defaultSaveFile = "save";
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.L))
+            {
+                Load();
+            }
+            else if (Input.GetKeyDown(KeyCode.S))
+            {
+                Save();
+            }
+        }
+
+        private void Load()
+        {
+            // Call the Saving System and tell it to load
+            GetComponent<SavingSystem>().Load(_defaultSaveFile);
+        }
+
+        private void Save()
+        {
+            // Call the Saving System and tell it to load
+            GetComponent<SavingSystem>().Save(_defaultSaveFile);
+        }
+    }
+}
