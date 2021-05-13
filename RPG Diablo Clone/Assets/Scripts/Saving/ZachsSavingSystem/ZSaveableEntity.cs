c7bd09c15bbfa3a7a89bfe8c22c5fe7a14bcd9a4
zachcantwell <bobbynfld@gmail.com>
2021-05-13 12:00:56 -0230

    More saving and loading
    Save and load positions for characters

--- RPG Diablo Clone/Assets/Scripts/Saving/ZachsSavingSystem/ZSaveableEntity.cs
+++ RPG Diablo Clone/Assets/Scripts/Saving/ZachsSavingSystem/ZSaveableEntity.cs
@@ -1,24 +1,60 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using RPG.Core;
+using UnityEditor;
 using UnityEngine;
+using UnityEngine.AI;
 
 namespace RPG.Saving
 {
+    [ExecuteAlways]
     public class ZSaveableEntity : MonoBehaviour
     {
+        [SerializeField] string _uniqueIdentifier = "";
+
+
         public string GetUniqueIdentifier()
         {
-            return "";
+            return _uniqueIdentifier;
         }
 
         public object CaptureState()
         {
-            return null;
+            return new SerializableVector3(transform.position);
         }
 
         public void RestoreState(object state)
         {
-            Dictionary<string, object> stateDict = (Dictionary<string, object>)state;
+            SerializableVector3 pos = (SerializableVector3)state;
+            GetComponent<NavMeshAgent>().enabled = false;
+            transform.position = pos.ToVector();
+            GetComponent<NavMeshAgent>().enabled = true;
+            GetComponent<ActionScheduler>().CancelCurrentAction();
+        }
+
+#if UNITY_EDITOR
+        void Update()
+        {
+            if (string.IsNullOrEmpty(gameObject.scene.path))
+            {
+                return;
+            }
+
+            if (Application.IsPlaying(gameObject) == true)
+            {
+                return;
+            }
+
+            SerializedObject obj = new SerializedObject(this);
+            SerializedProperty property = obj.FindProperty("_uniqueIdentifier");
+
+            if (property.stringValue == "")
+            {
+                property.stringValue = System.Guid.NewGuid().ToString();
+                obj.ApplyModifiedProperties();
+            }
+            print("Editing");
         }
     }
+#endif
 }
